5a50cab73a8ef6c1f1c0886b681006e77f5ed6a6
usagimaru <usagimaruma@gmail.com>
2021-09-10 03:37:18 +0900

    add `filesOwner` argument (fix protocol)

--- Sources/NibInstantiatable.swift
+++ Sources/NibInstantiatable.swift
@@ -25,7 +25,7 @@ public protocol NibInstantiatable: View {
 	static func nib(in bundle: Bundle?) -> Nib
 	
 	func loadOwnedNib(_ bundle: Bundle?)
-	static func loadUnownedNib(_ bundle: Bundle?) -> Self
+	static func loadUnownedNib(_ bundle: Bundle?, filesOwner: Any?) -> Self
 	
 	func didViewLoadedFromNib()
 
